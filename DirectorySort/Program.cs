20bca677b76329ce805a44bdae3aaebdd83a78b9
Swanchick <61909965+Swanchick@users.noreply.github.com>
2023-06-12 01:50:16 +0300

    Add project files.

---
+++ DirectorySort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+
+
+namespace DirectorySort
+{
+    class DirectorySort
+    {
+        private static string path = "D:\\Download\\";
+
+        private static void Main(string[] args)
+        {
+            DirectoryChecker();
+        }
+
+        private static void Update()
+        {
+            while (true)
+            {
+                DirectoryChecker();
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static void DirectoryChecker()
+        {
+            string[] files = Directory.GetFiles(path);
+
+            foreach (string file in files)
+            {
+                string fileName = Utils.GetFileNameFromPath(file);
+            }
+        }
+    }
+
+
+}
---
+++ DirectorySort/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectorySort
+{
+    internal class Utils
+    {
+        public static string GetFileNameFromPath(string path)
+        {
+            string[] strings = path.Split('\\');
+
+            string fileName = strings[strings.Length - 1];
+
+            return fileName;
+        }
+
+        public static string GetExtension(string fileName)
+        {
+            string[] fileNameSplited = fileName.Split('.');
+
+            if (fileNameSplited.Length != 2) return "";
+
+            return fileNameSplited[1];
+        }
+    }
+}
